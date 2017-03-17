d89622b05478c14285541a59fc14163fd3515719
Seth Berrier <berriers@uwstout.edu>
2017-03-17 16:59:57 -0500

    Stub React App file

---
+++ app/ReactApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createComponent('div', null, 'Hello Julianne!'),
+  document.getElementById('app-root')
+);
